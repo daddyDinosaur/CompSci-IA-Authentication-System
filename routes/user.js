5592b2205959bc73e3331204a9a7bd4f88ae2b74
Snow7yt <112722151+Snow7yt@users.noreply.github.com>
2023-04-20 08:27:02 +0200

    Update user.js
    fixing route / spelling mistakes

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const { User } = require('../models/user');
-const security = require('../api/security');
+const security = require('../routes/security');
 
 
 
