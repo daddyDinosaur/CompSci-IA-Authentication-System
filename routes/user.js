63714b65939a4e8a173244ba87936fd264496d64
Snow7yt <112722151+Snow7yt@users.noreply.github.com>
2023-04-20 08:26:10 +0200

    Update user.js
    Mistake

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const { User } = require('../models/schemas');
+const { User } = require('../models/user');
 const security = require('../api/security');
 
 
