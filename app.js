aef1f7e6271cc685fbbd83059db86b9458b1adcd
Snow7yt <112722151+Snow7yt@users.noreply.github.com>
2023-05-19 08:53:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.set('trust proxy', true)
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('views'));
 app.set('view engine', 'ejs');
 
 // Database
