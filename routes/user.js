c826149f03b7f0710664e622b719a4edbc1c4e4f
daddyDinosaur <39770979+daddyDinosaur@users.noreply.github.com>
2023-06-06 18:48:59 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const app = require('../app');
+const security = require('./security');
 
 router.get('/api/user', security, async (req, res) => {
     const users = await User.find().sort('name');
