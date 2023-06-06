9e1b58f68df195fe3862563c3b92d7b594e499f9
daddyDinosaur <39770979+daddyDinosaur@users.noreply.github.com>
2023-06-06 18:47:44 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -4,16 +4,21 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const app = require('../app');
 
-router.get('/api/user', async (req, res) => {
-    try {
-        const theUsers = await User.find();
-        const encrpyUsers = jwt.sign({users: theUsers}, {key: app.get('privSecret'), passphrase: 'GamingChairOnTop'}, { algorithm: 'RS256' , expiresIn: '5m' });
-        res.json(encrpyUsers);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'An error occurred' });
-    }
+router.get('/api/user', security, async (req, res) => {
+    const users = await User.find().sort('name');
+    res.render('index', { users });
 });
+
+// router.get('/api/user', async (req, res) => {
+//     try {
+//         const theUsers = await User.find();
+//         const encrpyUsers = jwt.sign({users: theUsers}, {key: app.get('privSecret'), passphrase: 'GamingChairOnTop'}, { algorithm: 'RS256' , expiresIn: '5m' });
+//         res.json(encrpyUsers);
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json({ error: 'An error occurred' });
+//     }
+// });
 
 
 module.exports = router;
