d9d52c4c717078df0dc75a93eb26d7cb172b67cb
Snow7yt <112722151+Snow7yt@users.noreply.github.com>
2023-06-06 09:40:14 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -5,9 +5,15 @@ const jwt = require('jsonwebtoken');
 const app = require('../app');
 
 router.get('/api/user', async (req, res) => {
-    const theUsers = await User.find();
-    const encrpyUsers = jwt.sign({users: theUsers}, {key: app.get('privSecret'), passphrase: 'GamingChairOnTop'}, { algorithm: 'RS256' , expiresIn: '5m' });
-    res.json(encrpyUsers);
+    try {
+        const theUsers = await User.find();
+        const encrpyUsers = jwt.sign({users: theUsers}, {key: app.get('privSecret'), passphrase: 'GamingChairOnTop'}, { algorithm: 'RS256' , expiresIn: '5m' });
+        res.json(encrpyUsers);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An error occurred' });
+    }
 });
+
 
 module.exports = router;
