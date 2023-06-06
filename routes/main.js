6da2c2f0c11e8e2a1f15ff918dd0c2d8c8ac3a9b
Snow7yt <112722151+Snow7yt@users.noreply.github.com>
2023-06-06 09:23:48 +0200

    Small changes to try and fix crashing

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
 const User = require('../models/user');
-const security = require('../routes/security');
+const security = require('./security');
 
 router.get('/', security, async (req, res) => {
     const users = await User.find().sort('name');
--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
 const User = require('../models/user');
-const security = require('../routes/security');
-const {app} = require('../app');
+const jwt = require('jsonwebtoken');
+const app = require('../app');
 
-app.get('/api/users', security, async (req, res) => {
+router.get('/api/user', async (req, res) => {
     const theUsers = await User.find();
-    
     const encrpyUsers = jwt.sign({users: theUsers}, {key: app.get('privSecret'), passphrase: 'GamingChairOnTop'}, { algorithm: 'RS256' , expiresIn: '5m' });
-
     res.json(encrpyUsers);
 });
 
