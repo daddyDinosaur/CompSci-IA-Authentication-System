17e7a2bf00b04e54be931e6b56f31e4f89e2daf5
daddyDinosaur <39770979+daddyDinosaur@users.noreply.github.com>
2023-06-07 21:05:29 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,21 @@
 const express = require('express');
-const router = express.Router();
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const security = require('./security');
 
 module.exports = (app) => {
-  router.get('/', async (req, res) => {
-      try {
-          const theUsers = await User.find();
-          const encrpyUsers = jwt.sign({users: theUsers}, {key: app.get('privSecret'), passphrase: 'GamingChairOnTop'}, { algorithm: 'RS256' , expiresIn: '5m' });
-          res.json(encrpyUsers);
-      } catch (err) {
-          console.error(err);
-          res.status(500).json({ error: 'An error occurred' });
-      }
-  });
+    const router = express.Router();
 
-  return router;
+    router.get('/', async (req, res) => {
+        try {
+            const theUsers = await User.find();
+            const encrpyUsers = jwt.sign({users: theUsers}, {key: app.get('privSecret'), passphrase: 'GamingChairOnTop'}, { algorithm: 'RS256' , expiresIn: '5m' });
+            res.json(encrpyUsers);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'An error occurred' });
+        }
+    });
+
+    return router;
 };
