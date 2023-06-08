97a7e0b7798c7ce315f673012a0ff0f101ac3197
daddyDinosaur <39770979+daddyDinosaur@users.noreply.github.com>
2023-06-08 14:15:05 +0200

    attempting to add security check

--- routes/security.js
+++ routes/security.js
@@ -2,6 +2,29 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const { User } = require('../models/user');
-const security = require('../routes/security');
+const fs = require('fs');
 
-module.exports = router;
+const publicKey = fs.readFileSync(process.env.PUB_KEY_PATH, 'utf8');
+
+const checkApiKey = async (req, res, next) => {
+    const token = req.cookies.authToken;
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized No Token' });
+    }
+    try {
+        const decoded = jwt.verify(token, publicKey , { algorithms: 'RS256' });
+        
+        const user = await User.findById(decoded.userId);
+
+        if (!user) {
+            return res.status(401).json({ message: 'Unauthorized Invalid User' });
+        }
+
+        next();
+
+    } catch (err) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+};
+
+module.exports = checkApiKey;
--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 
 const privateKey = fs.readFileSync(process.env.PRIV_KEY_PATH, 'utf8');
 
-router.get('/', async (req, res) => {
+router.get('/', security, async (req, res) => {
     try {
         const theUsers = await User.find();
         const encrpyUsers = jwt.sign({ users: theUsers }, { key: privateKey, passphrase: process.env.PASSPHRASE }, { algorithm: 'RS256', expiresIn: '5m' });
