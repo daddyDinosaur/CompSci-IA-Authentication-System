e69dcd61310816eb3d15e47a87f748b909a1039d
daddyDinosaur <39770979+daddyDinosaur@users.noreply.github.com>
2023-06-08 16:01:24 +0200

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -8,6 +8,8 @@ const privateKey = fs.readFileSync(process.env.PRIV_KEY_PATH, 'utf8');
 
 router.get('/', async (req, res) => {
     try {
+        console.log(req.query);
+
         const { email, password } = req.query;
 
         const user = await User.findOne({ email: email });
