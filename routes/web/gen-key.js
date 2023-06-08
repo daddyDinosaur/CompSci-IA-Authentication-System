8facf1116a0430dde178b482ec6d49953d782976
daddyDinosaur <39770979+daddyDinosaur@users.noreply.github.com>
2023-06-08 22:38:12 +0200

    remove useless dependency

--- routes/web/gen-key.js
+++ routes/web/gen-key.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
 const { SubKey } = require('../../models/subKey');
 const security = require('./security');
 const jwt = require('jsonwebtoken');
