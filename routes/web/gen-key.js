6cefd4dda465d113cacee87351840b30bf96d7e7
daddyDinosaur <39770979+daddyDinosaur@users.noreply.github.com>
2023-06-10 17:56:19 +0200

    fixed requires

--- routes/web/gen-key.js
+++ routes/web/gen-key.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const router = express.Router();
 const { SubKey } = require('../../models/subKey');
-const { checkApiKey, isAdmin } = require('./security');
+const { checkApiKey, isAdmin } = require('../api/security');
 const jwt = require('jsonwebtoken');
--- routes/web/main.js
+++ routes/web/main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { User } = require('../../models/user');
-const { checkApiKey, isAdmin } = require('./security');
+const { checkApiKey, isAdmin } = require('../api/security');
 
 router.get('/', checkApiKey, async (req, res) => {
     const users = await User.find().sort('name');
