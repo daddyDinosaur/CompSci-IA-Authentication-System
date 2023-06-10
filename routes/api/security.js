33596f182e189e841245ae9c599bb9804454613f
daddyDinosaur <39770979+daddyDinosaur@users.noreply.github.com>
2023-06-10 15:44:24 +0200

    Re-did security to check for expired token

--- routes/api/security.js
+++ routes/api/security.js
@@ -13,23 +13,35 @@ const checkApiKey = async (req, res, next) => {
         return res.status(401).json({ unauthorized: 'No Token' });
     }
     try {
-        const decoded = jwt.verify(token, publicKey, { algorithms: 'RS256' });
+        jwt.verify(token, publicKey, { algorithms: 'RS256' }, (err, decoded) => {
+            if (err) {
+                if (err instanceof jwt.TokenExpiredError) {
+                    return res.status(403).json({ error: "Token expired" });
+                } else {
+                    return res.status(403).json({ error: "Invalid token" });
+                }
+            }
         
-        const user = await User.findById(decoded.userId);
-
-        if (!user) {
-            return res.status(401).json({ unauthorized: 'Invalid User' });
+            handleUserAndIp(decoded.userId, req, res, next);
+        });
+        
+        async function handleUserAndIp(userId, req, res, next) {
+            const user = await User.findById(userId);
+        
+            if (!user) {
+                return res.status(401).json({ unauthorized: 'Invalid User' });
+            }
+        
+            var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        
+            const bannedIP = await User.findOne({$and: [{ lastIP: ip }, { banned: true }]});
+        
+            if (bannedIP) {
+                return res.status(401).json({ unauthorized: 'IP Banned' });
+            }
+        
+            next();
         }
-
-        var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-
-        const bannedIP = await User.findOne({$and: [{ lastIP: ip }, { banned: true }]});
-
-        if (bannedIP) {
-            return res.status(401).json({ unauthorized: 'IP Banned' });
-        }
-
-        next();
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'An error occurred' });
