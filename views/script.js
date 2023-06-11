fec9b905cadba1218bcaf0f0610e403a045ef917
daddyDinosaur <39770979+daddyDinosaur@users.noreply.github.com>
2023-06-11 13:45:21 +0200

    Visuals

--- views/script.js
+++ views/script.js
@@ -42,9 +42,9 @@ $(document).ready(function() {
                     <input class="input-field-special white-text" type="text" name="email" value="${user.email}">
                     <input class="input-field-special white-text" type="password" name="password" placeholder="New Password">
                     <input class="input-field-special white-text" type="text" name="role" value="${user.role}">
-                    <button id="update-button" type="button" class="white-text">Update User</button>
-                    <button id="ban-toggle-button" type="button" class="white-text">${user.banned ? 'Unban' : 'Ban'} User</button>
-                    <button id="delete-button" type="button" class="white-text">Delete User</button>
+                    <button id="update-button" type="button" class="more-info-special white-text">Update User</button>
+                    <button id="ban-toggle-button" type="button" class="more-info-special white-text">${user.banned ? 'Unban' : 'Ban'} User</button>
+                    <button id="delete-button" type="button" class="more-info-special white-text">Delete User</button>
                 </form>`;
             
                 $("#modal-content").html(html);
