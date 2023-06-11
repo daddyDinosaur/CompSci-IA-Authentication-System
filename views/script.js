7b3af214f6e771efe30a83f785aa0d2a37c5c45d
daddyDinosaur <39770979+daddyDinosaur@users.noreply.github.com>
2023-06-11 14:11:34 +0200

    Update script.js

--- views/script.js
+++ views/script.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
                     <input class="input-field-special white-text" type="text" name="username" value="${user.username}">
                     <input class="input-field-special white-text" type="text" name="email" value="${user.email}">
                     <input class="input-field-special white-text" type="password" name="password" placeholder="New Password">
-                    <input class="input-field-special white-text" type="text" name="role" value="${user.role}">
+                    <input class="input-field-special white-text" type="text" name="role" value="${user.role.toUpperCase()}">
                     <button id="update-button" type="button" class="more-info-special white-text">Update User</button>
                     <button id="ban-toggle-button" type="button" class="more-info-special white-text">${user.banned ? 'Unban' : 'Ban'} User</button>
                     <button id="delete-button" type="button" class="more-info-special white-text">Delete User</button>
