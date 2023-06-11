eee2d7d62475d104cc126fc72902d4acb80a36a9
daddyDinosaur <39770979+daddyDinosaur@users.noreply.github.com>
2023-06-11 18:18:10 +0200

    Update main.js

--- routes/web/main.js
+++ routes/web/main.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { User } = require('../../models/user');
+const { SubKey } = require('../../models/subKey');
 const { checkApiKey, isAdmin } = require('../api/security');
 
 router.get('/', checkApiKey, isAdmin, async (req, res) => {
     const users = await User.find().sort('name');
-    res.render('index', { users });
+    const keys = await SubKey.find().sort('name');
+    res.render('index', { users, keys });
 });
 
 module.exports = router;
