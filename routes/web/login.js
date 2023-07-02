d6edf39d63cb1afa35117648af6c7027ee147460
daddyDinosaur <39770979+daddyDinosaur@users.noreply.github.com>
2023-07-02 19:36:47 +0200

    Update login.js

--- routes/web/login.js
+++ routes/web/login.js
@@ -6,29 +6,23 @@ router.get('/', (req, res) => {
     res.render('login');
 });
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(401).json({ error: 'Missing Data' });
+    }
+
     try {
-        const { email, password } = req.body;
+        const loggedIn = await loginUser(email, password, req.ip);
 
-        if (!email || !password) {
-            return res.status(401).json({ error: 'Missing Data' });
+        if (loggedIn.error) {
+            res.status(loggedIn.status).json({ error: loggedIn.message });
+        } else {
+            res.redirect('/');
         }
-
-        try {
-            const loggedIn = await loginUser(email, password, req.ip);
-    
-            if (loggedIn.error) {
-                res.status(loggedIn.status).json({ error: loggedIn.message });
-            } else {
-                res.redirect('/');
-            }
-        } catch (err) {
-            next(err);
-        }
-
     } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'An error occurred' });
+        next(err);
     }
 });
 
