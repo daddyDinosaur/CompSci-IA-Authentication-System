6dc1d59dd94d9f915f5752d0b4c4dedd05000cea
daddyDinosaur <39770979+daddyDinosaur@users.noreply.github.com>
2023-07-02 19:36:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ const startApp = async () => {
     process.exit(1);
   }
 }
-
+ 
 app.use(function (err, req, res, next) {
   console.error(err.stack) 
   res.status(500).send('An error occurred!')
